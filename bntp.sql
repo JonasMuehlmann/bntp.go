8f1ac9aa8d047b0e38c0528be09289100b94eabd
jonas <jonasmuehlmann@protonmail.com>
2021-09-24 19:43:18 +0200

    chore: add DB creation script

---
+++ bntp.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Tag
+(
+    Id  INTEGER PRIMARY KEY NOT NULL,
+    Tag TEXT                NOT NULL UNIQUE
+);
+CREATE TABLE BookmarkType
+(
+    Id   INTEGER not null
+        primary key,
+    Type TEXT    not null
+        unique
+);
+CREATE TABLE Bookmark
+(
+    Id           INTEGER not null
+        primary key,
+    IsRead       INTEGER default 0,
+    Title        TEXT    not null,
+    Url          TEXT    not null
+        unique,
+    TimeAdded    TEXT    not null,
+    BookmarkTypeId
+        references BookmarkType (Id),
+    IsCollection INTEGER not null
+);
+CREATE TABLE Link
+(
+    Id           INTEGER PRIMARY KEY,
+    Source       Text,
+    Destination Text
+);
+CREATE TABLE DocumentType
+(
+    Id INTEGER PRIMARY KEY,
+    DocumentType Text
+);
+CREATE TABLE Document
+(
+    Id   INTEGER PRIMARY KEY,
+    Path Text,
+    DocumentTypeId REFERENCES DocumentType(Id)
+);
+CREATE TABLE BookmarkContext
+(
+    Id PRIMARY KEY,
+    BookmarkId REFERENCES Bookmark(Id),
+    BookmarkTypeId REFERENCES BookmarkType(Id)
+);
+CREATE TABLE DocumentContext
+(
+    Id PRIMARY KEY,
+    DocumentId REFERENCES Document (Id),
+    DocumentTypeId REFERENCES DocumentType (Id)
+);
