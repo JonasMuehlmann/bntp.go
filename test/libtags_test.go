35ff1cd18867121d1ed28c826321292fb1340cf7
jonas <jonasmuehlmann@protonmail.com>
2021-09-28 11:09:43 +0200

    test(`libtags`): add tests for `RemoveTag()`

--- test/libtags_test.go
+++ test/libtags_test.go
@@ -36,3 +36,42 @@ func TestAddTagEmptyTag(t *testing.T) {
 	err = libtags.AddTag(db, nil, "")
 	assert.NoError(t, err)
 }
+
+func TestRemoveTag(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "Foo")
+	assert.NoError(t, err)
+
+	err = libtags.DeleteTag(db, nil, "Foo")
+	assert.NoError(t, err)
+}
+
+func TestRemoveTagTransaction(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	transaction, err := db.Beginx()
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(nil, transaction, "Foo")
+	assert.NoError(t, err)
+
+	err = libtags.DeleteTag(nil, transaction, "Foo")
+	assert.NoError(t, err)
+
+	err = transaction.Commit()
+	assert.NoError(t, err)
+}
+
+func TestRemoveTagDoesNotExist(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "")
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "Bar")
+	assert.NoError(t, err)
+}
