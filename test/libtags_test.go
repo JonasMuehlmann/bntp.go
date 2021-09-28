a46e21359758422603fd90a55f32d3d59672fddd
jonas <jonasmuehlmann@protonmail.com>
2021-09-28 11:14:59 +0200

    test(`libtags`): add tests for `RenameTag()`

--- test/libtags_test.go
+++ test/libtags_test.go
@@ -75,3 +75,53 @@ func TestRemoveTagDoesNotExist(t *testing.T) {
 	err = libtags.AddTag(db, nil, "Bar")
 	assert.NoError(t, err)
 }
+
+func TestRenameTag(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "Foo")
+	assert.NoError(t, err)
+
+	err = libtags.RenameTag(db, nil, "Foo", "Bar")
+	assert.NoError(t, err)
+}
+
+func TestRenameTagTransaction(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	transaction, err := db.Beginx()
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(nil, transaction, "Foo")
+	assert.NoError(t, err)
+
+	err = libtags.RenameTag(nil, transaction, "Foo", "Bar")
+	assert.NoError(t, err)
+
+	err = transaction.Commit()
+	assert.NoError(t, err)
+}
+
+func TestRenameTagNoOld(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "")
+	assert.NoError(t, err)
+
+	err = libtags.RenameTag(db, nil, "XYZ", "Bar")
+	assert.NoError(t, err)
+}
+
+func TestRenameTagNewEmpty(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "")
+	assert.NoError(t, err)
+
+	err = libtags.RenameTag(db, nil, "Foo", "")
+	assert.NoError(t, err)
+}
