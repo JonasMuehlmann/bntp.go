09c6aed95eba0b5dbeb958251b33fee4362b6ede
jonas <jonasmuehlmann@protonmail.com>
2021-09-28 10:38:46 +0200

    test(`libtags`): add tests for `AddTag()`

--- test/libtags_test.go
+++ test/libtags_test.go
@@ -1,7 +1,38 @@
-package tests
+package test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/JonasMuehlmann/bntp.go/internal/libtags"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAddTag(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "Foo")
+	assert.NoError(t, err)
 }
-package test
+
+func TestAddTagTransaction(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	transaction, err := db.Beginx()
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(nil, transaction, "Foo")
+	assert.NoError(t, err)
+
+	err = transaction.Commit()
+	assert.NoError(t, err)
+}
+
+func TestAddTagEmptyTag(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libtags.AddTag(db, nil, "")
+	assert.NoError(t, err)
+}
