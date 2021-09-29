94466bc930a1cf683868790f9fa3365d580dbb88
jonas <jonasmuehlmann@protonmail.com>
2021-09-29 08:56:44 +0200

    test(`liblinks`): add tests for `RemoveLink()`

--- test/liblinks_test.go
+++ test/liblinks_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// ######@######
+// #############
 // # AddLink() #
 // #############
 func TestAddLink(t *testing.T) {
@@ -63,5 +63,61 @@ func TestAddLinkSelfReference(t *testing.T) {
 	assert.NoError(t, err)
 
 	err = liblinks.AddLink(db, nil, "Foo", "Foo")
+	assert.Error(t, err)
+}
+
+// ################
+// # RemoveLink() #
+// ################
+func TestRemoveLink(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo", "Bar")
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo2", "Bar2")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo2")
+	assert.NoError(t, err)
+
+	err = liblinks.RemoveLink(db, nil, "Foo", "Foo2")
+	assert.NoError(t, err)
+}
+
+func TestRemoveLinkSourceDoesNotExist(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo2", "Bar2")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo2")
+	assert.Error(t, err)
+
+	err = liblinks.RemoveLink(db, nil, "Bar", "Foo2")
+	assert.Error(t, err)
+}
+
+func TestRemoveLinkDestionationDoesNotExist(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo", "Bar")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo2")
+	assert.Error(t, err)
+
+	err = liblinks.RemoveLink(db, nil, "Foo", "Bar")
+	assert.Error(t, err)
+}
+
+func TestRemoveLinkNoneExist(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = liblinks.RemoveLink(db, nil, "Foo", "Foo2")
 	assert.Error(t, err)
 }
