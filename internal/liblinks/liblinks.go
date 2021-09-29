514babf524d3a0e2649c89678648bff59dda3f09
jonas <jonasmuehlmann@protonmail.com>
2021-09-29 08:04:29 +0200

    feat(`liblinks`): implement `GetIdFromDocument()`

--- internal/liblinks/liblinks.go
+++ internal/liblinks/liblinks.go
@@ -54,6 +54,54 @@ func GetIdFromType(dbConn *sqlx.DB, transaction *sqlx.Tx, type_ string) (int, er
 	return typeId, nil
 }
 
+func GetIdFromDocument(dbConn *sqlx.DB, transaction *sqlx.Tx, documentPath string) (int, error) {
+	stmt := `
+        SELECT
+            Id
+        FROM
+            Document
+        WHERE
+            Path = ?;
+    `
+
+	var statement *sqlx.Stmt
+	var err error
+
+	if transaction != nil {
+		statement, err = transaction.Preparex(stmt)
+
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		statement, err = dbConn.Preparex(stmt)
+
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	var documentId int
+
+	err = statement.Get(documentId, documentPath)
+
+	if err != nil {
+		return 0, err
+	}
+
+	err = statement.Close()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return documentId, nil
+}
+
 // AddLink adds a link from source to destination to the DB.
 // Passing a transaction is optional.
 func AddLink(dbConn *sqlx.DB, transaction *sqlx.Tx, source string, destination string) error {
