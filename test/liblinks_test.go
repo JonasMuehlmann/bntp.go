c53817b832b9f96cb1a7306a6e14179d32bb4b11
jonas <jonasmuehlmann@protonmail.com>
2021-09-29 09:55:51 +0200

    test(`liblinks`): add tests for `ListLinks()`

--- test/liblinks_test.go
+++ test/liblinks_test.go
@@ -121,3 +121,64 @@ func TestRemoveLinkNoneExist(t *testing.T) {
 	err = liblinks.RemoveLink(db, nil, "Foo", "Foo2")
 	assert.Error(t, err)
 }
+
+// ###############
+// # ListLinks() #
+// ###############
+func TestListLinksNoneExist(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	_, err = liblinks.ListLinks(db, "Foo")
+	assert.Error(t, err)
+}
+
+func TestListLinksOne(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo", "Bar")
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo2", "Bar")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo2")
+	assert.NoError(t, err)
+
+	links, err := liblinks.ListLinks(db, "Foo")
+	assert.NoError(t, err)
+	assert.Len(t, links, 1)
+	assert.Equal(t, "Foo2", links[0])
+}
+
+func TestListLinksMany(t *testing.T) {
+	db, err := GetDB(t)
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo", "Bar")
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo2", "Bar")
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo3", "Bar")
+	assert.NoError(t, err)
+
+	err = libdocuments.AddDocument(db, nil, "Foo4", "Bar")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo2")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo3")
+	assert.NoError(t, err)
+
+	err = liblinks.AddLink(db, nil, "Foo", "Foo4")
+	assert.NoError(t, err)
+
+	links, err := liblinks.ListLinks(db, "Foo")
+	assert.NoError(t, err)
+	assert.Len(t, links, 3)
+	assert.Equal(t, []string{"Foo2", "Foo3", "Foo4"}, links)
+}
