9890adca072ac09c14ae1bda7ef7e7f121ef0f6d
jonas <jonasmuehlmann@protonmail.com>
2021-09-30 10:40:36 +0200

    !refactor(`helpers`): rename `GetIdFromType()`

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -9,6 +9,50 @@ func GetIdFromType(dbConn *sqlx.DB, transaction *sqlx.Tx, type_ string) (int, er
         FROM
             DocumentType
         WHERE
+            DocumentType = ?;
+    `
+
+	var statement *sqlx.Stmt
+	var err error
+
+	if transaction != nil {
+		statement, err = transaction.Preparex(stmt)
+
+		if err != nil {
+			return -1, err
+		}
+	} else {
+		statement, err = dbConn.Preparex(stmt)
+
+		if err != nil {
+			return -1, err
+		}
+	}
+
+	if err != nil {
+		return -1, err
+	}
+
+	typeId := -1
+
+	_ = statement.Get(&typeId, type_)
+
+	err = statement.Close()
+
+	if err != nil {
+		return -1, err
+	}
+
+	return typeId, nil
+}
+
+func GetIdFromBookmarkType(dbConn *sqlx.DB, transaction *sqlx.Tx, type_ string) (int, error) {
+	stmt := `
+        SELECT
+            Id
+        FROM
+            BookmarkType
+        WHERE
             Type = ?;
     `
 
@@ -44,6 +88,50 @@ func GetIdFromType(dbConn *sqlx.DB, transaction *sqlx.Tx, type_ string) (int, er
 	}
 
 	return typeId, nil
+}
+
+func GetIdFromBookmark(dbConn *sqlx.DB, transaction *sqlx.Tx, url string) (int, error) {
+	stmt := `
+        SELECT
+            Id
+        FROM
+            Bookmark
+        WHERE
+            Url = ?;
+    `
+
+	var statement *sqlx.Stmt
+	var err error
+
+	if transaction != nil {
+		statement, err = transaction.Preparex(stmt)
+
+		if err != nil {
+			return -1, err
+		}
+	} else {
+		statement, err = dbConn.Preparex(stmt)
+
+		if err != nil {
+			return -1, err
+		}
+	}
+
+	if err != nil {
+		return -1, err
+	}
+
+	bookmarkId := -1
+
+	_ = statement.Get(&bookmarkId, url)
+
+	err = statement.Close()
+
+	if err != nil {
+		return -1, err
+	}
+
+	return bookmarkId, nil
 }
 
 func GetIdFromDocument(dbConn *sqlx.DB, transaction *sqlx.Tx, documentPath string) (int, error) {
