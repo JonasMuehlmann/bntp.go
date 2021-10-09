f4d8b8ff0e455bb7636ba3823b071ba0de924ffd
jonas <jonasmuehlmann@protonmail.com>
2021-10-09 15:36:13 +0200

    test(`libdocuments`): add test for `RemoveLink()`

--- test/libdocuments_test.go
+++ test/libdocuments_test.go
@@ -876,3 +876,64 @@ func TestAddBacklinkToFileOneBacklink(t *testing.T) {
 	err = libdocuments.AddBacklink(filePath, "foo")
 	assert.NoError(t, err)
 }
+
+// ################
+// # RemoveLink() #
+// ################
+func RemoveLinkFromFileEmpty(t *testing.T) {
+	filePath := filepath.Join(testDataTempDir, t.Name())
+
+	file, err := os.Create(filePath)
+	assert.NoError(t, err)
+
+	document := ""
+	_, err = file.WriteString(document)
+	assert.NoError(t, err)
+
+	err = libdocuments.RemoveLink(filePath, "foo")
+	assert.Error(t, err)
+}
+
+func RemoveLinkFromFileHeaderOnly(t *testing.T) {
+	filePath := filepath.Join(testDataTempDir, t.Name())
+
+	file, err := os.Create(filePath)
+	assert.NoError(t, err)
+
+	document := "# Links"
+	_, err = file.WriteString(document)
+	assert.NoError(t, err)
+
+	err = libdocuments.RemoveLink(filePath, "foo")
+	assert.NoError(t, err)
+}
+
+func RemoveLinkFromFileNoMatch(t *testing.T) {
+	filePath := filepath.Join(testDataTempDir, t.Name())
+
+	file, err := os.Create(filePath)
+	assert.NoError(t, err)
+
+	document := `# Backlinks
+    - ()[foo]`
+	_, err = file.WriteString(document)
+	assert.NoError(t, err)
+
+	err = libdocuments.RemoveLink(filePath, "bar")
+	assert.NoError(t, err)
+}
+
+func RemoveLinkFromFile(t *testing.T) {
+	filePath := filepath.Join(testDataTempDir, t.Name())
+
+	file, err := os.Create(filePath)
+	assert.NoError(t, err)
+
+	document := `# Backlinks
+    - ()[foo]`
+	_, err = file.WriteString(document)
+	assert.NoError(t, err)
+
+	err = libdocuments.RemoveLink(filePath, "foo")
+	assert.NoError(t, err)
+}
