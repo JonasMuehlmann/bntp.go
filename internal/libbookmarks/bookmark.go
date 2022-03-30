b90659c990112e92880a130b1fc6184eb3b4dcad
jonas <jonasmuehlmann@protonmail.com>
2022-03-30 09:31:59 +0200

    chore(types): add json tags to `Bookmark` struct

--- internal/libbookmarks/bookmark.go
+++ internal/libbookmarks/bookmark.go
@@ -4,12 +4,12 @@ import "database/sql"
 
 // Bookmark is a code side representation of DB bookmarks.
 type Bookmark struct {
-	Title        sql.NullString `db:"Title"`
-	Url          string         `db:"Url"`
-	TimeAdded    string         `db:"TimeAdded"`
-	Type         sql.NullString `db:"Type"`
+	Title        sql.NullString `json:"title" db:"Title"`
+	Url          string         `json:"url" db:"Url"`
+	TimeAdded    string         `json:"time_added" db:"TimeAdded"`
+	Type         sql.NullString `json:"type" db:"Type"`
+	Id           int            `json:"id" db:"Id"`
+	IsRead       bool           `json:"is_read" db:"IsRead"`
+	IsCollection sql.NullBool   `json:"is_collection" db:"IsCollection"`
 	Tags         []string
-	Id           int          `db:"Id"`
-	IsRead       bool         `db:"IsRead"`
-	IsCollection sql.NullBool `db:"IsCollection"`
 }
