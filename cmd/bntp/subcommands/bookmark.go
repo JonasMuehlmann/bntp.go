b4c3a57d3bda5220ffb3de10c1b3b96eb55c453c
jonas <jonasmuehlmann@protonmail.com>
2022-03-30 08:11:57 +0200

    feat(cli): document subcommand `bookmark`

--- cmd/bntp/subcommands/bookmark.go
+++ cmd/bntp/subcommands/bookmark.go
@@ -6,9 +6,27 @@ var usageBookmark string = `bntp bookmark - Interact with bookmarks.
 
 Usage:
     bntp bookmark -h | --help
+    bntp bookmark -l [--filter]
 
 Options:
-    -h --help     Show this screen.
+    -h --help               Show this screen.
+    -i --import             Import bookmarks.
+    -e --export             Export bookmarks.
+    -l --list               List bookmarks.
+    --filter                A filter to apply when searching for bookmarks.
+    --add-type              Add a bookmark type.
+    --remove-type           Remove a bookmark type.
+    --list-types            List the bookmark types.
+    -a --add                Add a bookmark.
+    -r --rmove              Remove a bookmark.
+    -e --edit               Edit a bookmark.
+    --edit-is-read          Change the is read status of a bookmark.
+    --edit-title            Change the title of a bookmark.
+    --edit-url              Change the url of a bookmark.
+    --edit-type             Change the type of a bookmark.
+    --edit-is-collection    Change the is collection status of a bookmark.
+    --add-tag               Add a tag to a bookmark.
+    --remove-tag            Remove a tag from a bookmark.
 `
 
 func BookmarkMain() {
