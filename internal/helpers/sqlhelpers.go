c8d9afe772cdf2754b95b5c62c03a988f2b4630e
jonas <jonasmuehlmann@protonmail.com>
2022-03-30 09:32:25 +0200

    feat(helpers): add method to wraped execution of named sql statement

--- internal/helpers/sqlhelpers.go
+++ internal/helpers/sqlhelpers.go
@@ -46,6 +46,48 @@ func SqlExecute(dbConn *sqlx.DB, transaction *sqlx.Tx, stmt string, args ...inte
 	return lastInsertedId, numAffectedRows, nil
 }
 
+// SqlExecuteNamed is a wrapper used to run a prepared statement stmt with the specified args.
+func SqlExecuteNamed(dbConn *sqlx.DB, transaction *sqlx.Tx, stmt string, struct_ interface{}) (int64, int64, error) {
+	var statement *sqlx.NamedStmt
+
+	var err error
+
+	if transaction != nil {
+		statement, err = transaction.PrepareNamed(stmt)
+
+		if err != nil {
+			return 0, 0, err
+		}
+	} else {
+		statement, err = dbConn.PrepareNamed(stmt)
+
+		if err != nil {
+			return 0, 0, err
+		}
+	}
+
+	res, err := statement.Exec(struct_)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	err = statement.Close()
+
+	if err != nil {
+		return 0, 0, err
+	}
+
+	numAffectedRows, err := res.RowsAffected()
+	if err != nil {
+		return 0, 0, err
+	}
+	lastInsertedId, err := res.LastInsertId()
+	if err != nil {
+		return 0, 0, err
+	}
+	return lastInsertedId, numAffectedRows, nil
+}
+
 func GetDefaultDB() (*sqlx.DB, error) {
 	db, err := sqlx.Open("sqlite3", "~/Documents/bntp/bookmarks.db?_foreign_keys=1")
 	if err != nil {
