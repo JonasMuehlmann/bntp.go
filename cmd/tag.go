4066a80a7b9d5ca1cc59eda028ef6b08249ed7fe
jonas <jonasmuehlmann@protonmail.com>
2022-04-17 19:12:50 +0200

    chore: add basic structure of new CLIs `tag` subcommand

---
+++ cmd/tag.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// tagCmd represents the tag command
+var tagCmd = &cobra.Command{
+	Use:   "tag",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("tag called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tagCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// tagCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// tagCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
