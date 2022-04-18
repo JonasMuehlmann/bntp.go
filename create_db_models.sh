005f6a50a8812dafd7ece7aeb552835a09b29e0c
jonas <jonasmuehlmann@protonmail.com>
2022-04-18 20:19:12 +0200

    chore: make sql model creation script more robust

--- create_db_models.sh
+++ create_db_models.sh
@@ -3,10 +3,12 @@
 DBS=("sqlite3" "psql" "mssql" "mysql")
 
 for db in "${DBS[@]}"; do
-    new_dir=models/$db
+    new_dir=./models/$db
     mkdir -p $new_dir
 
     sqlboiler --output $new_dir $db
-    xargs sed -i 's/t.Parallel()//' $new_dir/*
+    sed -i 's/t.Parallel()//' $new_dir/*
+    # Only needed for $b=sqlite3
+    cp bntp_sqlite.db $new_dir
     go test -v $new_dir
 done
