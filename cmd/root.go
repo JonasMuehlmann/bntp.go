3b459285612e503cc8af335371657d9042434cf6
jonas <jonasmuehlmann@protonmail.com>
2022-04-18 16:40:33 +0200

    feat(config): add more config search paths and allow using more config formats

--- cmd/root.go
+++ cmd/root.go
@@ -45,14 +45,11 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".bntp.go" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".bntp.go")
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("$HOME/.config/bntp/")
+		viper.AddConfigPath("$HOME/.config/")
+		viper.AddConfigPath("$HOME/")
+		viper.SetConfigName("bntprc")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
@@ -60,5 +57,8 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Fprintf(os.Stderr, "%v, allowed extensions/formats: %v\n", err.Error(), viper.SupportedExts)
+		os.Exit(0)
 	}
 }
