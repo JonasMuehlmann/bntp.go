4142c9f0ec359845e7a7570e992500bc83ef041d
jonas <jonasmuehlmann@protonmail.com>
2022-04-23 21:05:49 +0200

    refactor(architecture): copy generated db models into respective libraries `repository`package

--- create_db_models.sh
+++ create_db_models.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Enable ** glob
+shopt -s globstar
+
 go install github.com/volatiletech/sqlboiler/v4@latest
 go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
 go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql@latest
@@ -17,9 +20,6 @@ for db in "${DBS[@]}"; do
     if [[ $db == "sqlite3" ]]; then
         cp bntp_test.db $new_dir
     fi
-    # if [[ $db == "mysql" ]]; then
-    #     sed -i 's/ssl-mode/ssl/' $new_dir/*
-    # fi
     if [[ $db == "mssql" ]]; then
         cp schema/bntp_sqlserver_test.sql tables_schema.sql
         cp schema/bntp_sqlserver_test.sql $new_dir/tables_schema.sql
@@ -28,3 +28,28 @@ for db in "${DBS[@]}"; do
     go get -t github.com/JonasMuehlmann/bntp.go/$new_dir
     go test -v $new_dir
 done
+
+REPOSITORIES=(libbookmarks libdocuments liblinks libtags)
+
+# Users won't need these tests anymore
+rm ./models/**/*_test.go
+
+# Copy generated models to libraries
+for repo in "${REPOSITORIES[@]}"; do
+    for db in "${DBS[@]}"; do
+        repo_dir="./pkg/$repo/repository/$db"
+        mkdir -p "$repo_dir"
+        # Not sure if these are mandatory, but keeping the mcan't hurt
+        cp -t "$repo_dir/" ./models/$db/{boil_queries,boil_types,boil_table_names,boil_view_names,*_upsert}.go
+        # Strip prefix lib
+        tmp=${repo#lib}
+        # Strip suffix s
+        tmp=${tmp%s}
+        mv ./models/$db/*$tmp*.go "$repo_dir/"
+
+        sed -i "s/package models/ package ${tmp}_repository/g" "$repo_dir/"*.go
+    done
+done
+
+# Remove temp dir of generation
+rm -r models
