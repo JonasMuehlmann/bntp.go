413621b4ed9af0200dd0b1b1c66ade05882bba48
jonas <jonasmuehlmann@protonmail.com>
2022-06-05 18:06:08 +0200

    ci(code_gen): add missing calls to code generators

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 //go:generate go run ./tools/generate_config_key_constants
 //go:generate go run ./tools/generate_external_cli_documentation
 //go:generate go run ./tools/generate_domain_models
-//go:generate go run ./tools/generate_sql_repositories
 //go:generate go run ./tools/generate_repository_interfaces
+//go:generate go run ./tools/generate_sql_repository_filter_converters
+//go:generate go run ./tools/generate_sql_repository_model_converters
+//go:generate go run ./tools/generate_sql_repository_updaters_converters
+//go:generate go run ./tools/generate_sql_repositories
 
 import "github.com/JonasMuehlmann/bntp.go/cmd"
 
