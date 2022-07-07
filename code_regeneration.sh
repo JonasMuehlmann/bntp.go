3b4bd04b9d3dea75f9126db071440976267420c3
jonas <jonasmuehlmann@protonmail.com>
2022-07-07 13:00:24 +0200

    ci:(code_generation): extend code generation script

--- code_regeneration.sh
+++ code_regeneration.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
 rm model/repository/**/*_repository.go || true
-go run ./tools/generate_sql_repositories
-go run ./tools/generate_repository_interfaces
+
+go run ./tools/schema_converter
+go run ./tools/generate_external_cli_documentation
 go run ./tools/generate_domain_models
+go run ./tools/generate_repository_interfaces
+go run ./tools/generate_sql_repositories
 go run ./tools/generate_repository_tests
+
 git restore model/repository/document_content_repository.go model/repository/fs/document_content_repository.go
--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-//go:generate go run ./tools/schema_converter
-//go:generate go run ./tools/generate_external_cli_documentation
-//go:generate go run ./tools/generate_domain_models
-//go:generate go run ./tools/generate_repository_interfaces
-//go:generate go run ./tools/generate_sql_repositories
-//go:generate go run ./tools/generate_repository_tests
-
 import (
 	"github.com/JonasMuehlmann/bntp.go/bntp/backend"
 	"github.com/JonasMuehlmann/bntp.go/cmd"
